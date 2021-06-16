9970fe027f9674e59a6a891273814dc17a984867
Mohamed Fahmi <m.fahmi.abdo@gmail.com>
2021-06-16 22:54:35 +0300

    [FIX] Fix module label

--- training_center/__manifest__.py
+++ training_center/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "training_center",
+    'name': "Training Center",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
