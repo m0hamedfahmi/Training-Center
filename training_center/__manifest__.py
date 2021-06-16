0c85b68a41692c5c36400141d8685764e243ee0c
Mohamed Fahmi <m.fahmi.abdo@gmail.com>
2021-06-16 23:03:38 +0300

    [FIX] A Fix for a reported issue.

--- training_center/__manifest__.py
+++ training_center/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Training Center",
+    'name': "Training Center (Fixed)",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
