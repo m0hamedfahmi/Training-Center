b2a5f65258120f3a1796e433a41abf1547b65664
Mohamed Fahmi <m.fahmi.abdo@gmail.com>
2021-06-17 00:21:25 +0300

    [ADD] Add the base for the model 'student.student' and the needed views, actions and menus.

--- training_center/models/models.py
+++ training_center/models/models.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+
+class Students(models.Model):
+    _name = 'student.student'
+    _rec_name = 'name'
+    _description = 'Students Model'
+
+    name = fields.Char(string="Student Name", required=True, )
+    birth_date = fields.Date(string="Birth Date", required=True, default=fields.Date.context_today)
 
