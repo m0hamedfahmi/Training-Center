428264951f7a2e4dc2bf152be91e10daf914e948
Mohamed Fahmi <m.fahmi.abdo@gmail.com>
2021-06-20 00:02:19 +0300

    [ADD] Extend the functionaity of the models by inheriting models

--- training_center/models/models.py
+++ training_center/models/models.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError, UserError
 
 
 class Students(models.Model):
     _name = 'student.student'
     _rec_name = 'name'
+    _inherit = ['image.mixin', 'mail.thread', 'mail.activity.mixin']
     _description = 'Students Model'
 
     name = fields.Char(string="Student Name", required=True, )
