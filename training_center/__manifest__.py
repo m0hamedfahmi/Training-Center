881362ac8d7d0187b173958d1600f880551c3481
Mohamed Fahmi <m.fahmi.abdo@gmail.com>
2021-06-30 23:21:49 +0300

    [ADD] Add updates.

--- training_center/__manifest__.py
+++ training_center/__manifest__.py
@@ -3,20 +3,20 @@
     'name': "Training Center",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
+        Short (1 phrase/line) summary of the module's
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "m.fahmy",
+    'website': "http://www.educenter.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Education',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
